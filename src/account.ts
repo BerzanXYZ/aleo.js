3c4c391ff6be2e5979f93c0877220b39e336e580
Berzan <berzan@tutanota.com>
2022-09-10 19:52:14 +0300

    create: account.ts

---
+++ src/account.ts
@@ -0,0 +1,33 @@
+// Provides an object that signs transactions on Aleo blockchain
+export class Account {
+    /**
+     * # Aleo.js
+     * ## `Account`
+     * - Provides an object that signs transactions on Aleo blockchain
+     * 
+     * ## Example
+     * ```ts
+     * // Declare a new Account object
+     * const account = new Account()
+     * ```
+     */
+    constructor() {
+        // TO BE DONE
+        // LEARN HOW TO CREATE AN ALEO ACCOUNT, INCLUDING PRIVATE KEY,  VIEW KEY, & PUBLIC KEY
+    }
+
+    /**
+     * # Aleo.js
+     * ## `generate`
+     * - Generates a new Aleo account 
+     * 
+     * ## Example
+     * ```ts
+     * // Generate a new Account object
+     * const account = Account.generate()        
+     * ```
+     */
+    generate(): Account {
+        return new Account()
+    }
+}
